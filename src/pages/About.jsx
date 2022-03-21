df426b21e5f95b34b81c566aec36d3ae5e71b424
FraneCaleta <frane.caleta@yahoo.com>
2022-03-21 23:04:19 +0100

    Routes added

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </>
+  );
+}
+
+export default About;

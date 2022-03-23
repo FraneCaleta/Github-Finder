7f0fc809b54a63a17071d5633a422be49f18c177
FraneCaleta <frane.caleta@yahoo.com>
2022-03-23 22:06:48 +0100

    Add Spinner component and GIF

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "../users/UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  return (
-    <div>UserResults</div>
-  )
+  const { users, loading } = useContext(GithubContext);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
-export default UserResults
+export default UserResults;

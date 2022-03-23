ef7fe61b8fd0ef5d271788021b7904357ca83b40
FraneCaleta <frane.caleta@yahoo.com>
2022-03-23 21:57:14 +0100

    Add env variable and users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import UserResults from "../components/users/UserResults";
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-    </div>
+    <>
+      <UserResults />
+    </>
   );
 }
 

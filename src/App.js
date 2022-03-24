e515de1ed3fca4e85d1160787f6291380e277fdd
FraneCaleta <frane.caleta@yahoo.com>
2022-03-24 18:49:17 +0100

    Alert component, reducer and context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Alert from "./components/layout/Alert";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main className="container mx-auto px-3 pb-12">
+          <Alert />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />

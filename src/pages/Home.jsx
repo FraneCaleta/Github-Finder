6485c01c14effff71a94e4bc98eb272919777f62
FraneCaleta <frane.caleta@yahoo.com>
2022-03-26 23:25:52 +0100

    Final refactoring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from "react";
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 function Home() {
   return (
     <>
+      <UserSearch />
       <UserResults />
     </>
   );

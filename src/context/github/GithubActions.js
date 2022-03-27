84c2b05bc4c7fa19595bbda44c41734bce044d06
FraneCaleta <frane.caleta@yahoo.com>
2022-03-27 13:33:25 +0200

    Finished App

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+  // You can add github token in .env file to get more available searches per hour
+  /* headers: { Authorization: `token ${GITHUB_TOKEN}` }, */
 });
 
 // Get search results
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ function About() {
     <>
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
-        A React app to search GitHub profiles and see profile details.
+        React application used to search Github users and display their
+        information in an awesome way!
       </p>
       <p className="text-lg text-gray-400">
         Version <span className="text-white">1.0.0</span>
